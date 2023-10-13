2200a90d0a4d831ab69ab038d139a2ea06f12eaa
AdrianRoger <adrianroge@gmail.com>
2023-10-12 23:45:43 -0300

    [feat]: Adicionar UML, DB, e connection JAVA
        modelo físico: script sql
        diagrama de classe: asta e pdf
        início de projeto: classe de conexão
    
     On branch development
     Changes to be committed:
    	new file:   BancoDeDados/skyline_fisico.sql
    	new file:   Diagrama_de_Classe/Class Diagram - Skyline.asta
    	new file:   Diagrama_de_Classe/Class Diagram - Skyline.asta.bak
    	new file:   Diagrama_de_Classe/Skyline_uml.pdf
    	new file:   Java_SE/.gitignore
    	new file:   Java_SE/Skyline/.classpath
    	new file:   Java_SE/Skyline/.gitignore
    	new file:   Java_SE/Skyline/.project
    	new file:   Java_SE/Skyline/.settings/org.eclipse.core.resources.prefs
    	new file:   Java_SE/Skyline/.settings/org.eclipse.jdt.core.prefs
    	new file:   Java_SE/Skyline/mysql-connector-j-8.1.0.jar
    	new file:   Java_SE/Skyline/src/br/com/skyline/connection/ConnectionMySQL.java

---
+++ BancoDeDados/skyline_fisico.sql
@@ -0,0 +1,57 @@
+drop database skyline;
+create database skyline;
+use skyline;
+
+create table if not exists CLIENTES(
+	cpf varchar(11) primary key,
+    nome varchar(50) not null,
+    telefone varchar(14),
+    email varchar(30) unique not null,
+    cep varchar(8) not null,
+    rua varchar(50) not null,
+    numero int not null,
+    complemento varchar(50),
+    ativo boolean not null
+);
+
+create table if not exists CIDADES(
+	id_cidade int primary key auto_increment,
+    cidade varchar(30) not null,
+    estado varchar(30) not null,
+    pais varchar(30) not null,
+    aeroporto varchar(50) not null
+);
+
+create table if not exists VOO(
+	id_voo int primary key auto_increment,
+    num_voo int unique not null,
+    comp_aerea varchar(30) not null,
+    assentos int not null,
+    preco_unit decimal(10,2) not null,
+    data_partida date not null,
+    FK_id_cidade_origem int not null,
+    FK_id_cidade_destino int not null,
+    constraint foreign key (FK_id_cidade_origem) references cidades(id_cidade),
+    constraint foreign key (FK_id_cidade_destino) references cidades(id_cidade)
+);
+
+create table if not exists RESERVAS(
+	id_reserva int primary key auto_increment,
+    data_reserva date not null,
+    num_pessoas int not null,
+    cancelada boolean not null,
+    FK_cpf_clientes varchar(11) not null,
+    FK_id_voo int not null,
+    constraint foreign key (FK_cpf_clientes) references clientes(cpf),
+    constraint foreign key (FK_id_voo) references voo(id_voo)
+);
+
+create table if not exists CONTATO(
+	id_contato int primary key auto_increment,
+    nome varchar(50) not null,
+    email varchar(30) not null,
+    telefone varchar(14) not null,
+    mensagem varchar(500) not null,
+    resolvido boolean not null
+);
+
---
+++ Java_SE/Skyline/src/br/com/skyline/connection/ConnectionMySQL.java
@@ -0,0 +1,32 @@
+package br.com.skyline.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionMySQL {
+	private static final String URL = "jdbc:mysql://localhost:3306/skyline";
+	private static final String USER= "root";
+	private static final String PASSWORD = "12345";
+	
+	public static Connection createConnectionMySQL() throws Exception{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		
+		return conn;
+		
+	}
+	
+	//main criado para testar se a conexão está funcionando
+	public static void main(String[] args) throws Exception {
+		//Recupoera uma conexão com o banco de dados
+		Connection conn = createConnectionMySQL();
+		
+		//testar a conexão
+		if(conn != null) {
+			System.out.println(conn + "\n\n *** Conexão realizada com sucesso ***");
+			conn.close();
+		}
+	}
+	
+}
